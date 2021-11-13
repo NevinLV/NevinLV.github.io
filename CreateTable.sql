f0d43abc9adf9215d7250421c573c311e7097d4c
n.lebedev.2019 <nikitalebedeff@gmail.com>
2021-11-14 02:05:46 +0700

    + SQLite script

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,62 +1,54 @@
 CREATE TABLE IF NOT EXISTS Category (
-  `id` INT NOT NULL,
-  `title` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  `id` INT PRIMARY KEY NOT NULL,
+  `title` VARCHAR(45) NOT NULL);
 
 
 
 CREATE TABLE IF NOT EXISTS User (
-  `id` INT NOT NULL,
+  `id` INT PRIMARY KEY NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  `password` VARCHAR(45) NOT NULL);
 
 
 
 CREATE TABLE IF NOT EXISTS Author (
-  `id` INT NOT NULL,
+  `id` INT PRIMARY KEY NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `User_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `User_id`));
+  `User_id` INT NOT NULL);
 
 
 
 CREATE TABLE IF NOT EXISTS Cours (
-  `id` INT NULL,
+  `id` INT PRIMARY KEY NOT NULL,
   `title` VARCHAR(80) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
   `date` DATE NOT NULL,
   `Category_id` INT NOT NULL,
-  `Author_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `Category_id`, `Author_id`));
+  `Author_id` INT NOT NULL);
 
 
 
 CREATE TABLE IF NOT EXISTS Img (
-  `id` INT NOT NULL,
+  `id` INT PRIMARY KEY NOT NULL,
   `path` VARCHAR(45) NOT NULL,
-  `Cours_id` INT NOT NULL,
-  PRIMARY KEY (`id`));
+  `Cours_id` INT NOT NULL);
 
 
 
 CREATE TABLE IF NOT EXISTS Video (
-  `id` INT NOT NULL,
+  `id` INT PRIMARY KEY NOT NULL,
   `path` VARCHAR(45) NOT NULL,
-  `Cours_id` INT NOT NULL,
-  PRIMARY KEY (`id`));
+  `Cours_id` INT NOT NULL);
 
 
 CREATE TABLE IF NOT EXISTS Tag (
-  `id` INT NOT NULL,
+  `id` INT PRIMARY KEY NOT NULL,
   `title` VARCHAR(45) NOT NULL,
   `Cours_id` INT NOT NULL,
-  `Cours_Category_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `Cours_id`, `Cours_Category_id`));
+  `Cours_Category_id` INT NOT NULL);
 
 
 CREATE TABLE IF NOT EXISTS Cours_User(
   `Cours_id` INT NOT NULL,
-  `User_id` INT NOT NULL,
-  PRIMARY KEY (`Cours_id`, `User_id`));
+  `User_id` INT NOT NULL);
